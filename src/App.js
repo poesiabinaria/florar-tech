bae2747d8a68def980cc0f0e399aec281e060508
Gabriel-G15\Gabriel <higabinario@gmail.com>
2025-01-13 08:09:56 -0300

    add section links and update some texts

--- src/App.js
+++ src/App.js
@@ -41,9 +41,15 @@ const App = () => {
       <header className="px-2 py-10">
         <nav className="container mx-auto flex items-center justify-between">
           <ul className="hidden md:flex items-center gap-16 uppercase font-extgralight text-xs basis-1/3">
-            <li className="opacity-60 hover:opacity-100">Processo</li>
-            <li className="opacity-60 hover:opacity-100">Portfolio</li>
-            <li className="opacity-60 hover:opacity-100">Depoimentos</li>
+            <li className="opacity-60 hover:opacity-100">
+              <a href="#work-process-section">Processo</a>
+            </li>
+            <li className="opacity-60 hover:opacity-100">
+              <a href="#portfolio-section">Portfolio</a>
+            </li>
+            <li className="opacity-60 hover:opacity-100">
+              <a href="#testmonials-section">Depoimentos</a>
+            </li>
           </ul>
 
           <div className="basis-1/3">
@@ -53,16 +59,27 @@ const App = () => {
           <div className="flex items-center justify-end gap-16 basis-1/3">
             <ul className="hidden md:flex items-center gap-16 uppercase font-extgralight text-xs basis-1/3">
               <li className="flex gap-2 items-center opacity-60 hover:opacity-100">
-                <div>LinkedIn</div> <img className="w-4" src={newTabIcon} />
+                <a
+                  href="https://www.linkedin.com/in/gabriel-mendes-macedo"
+                  target="_blank"
+                >
+                  LinkedIn
+                </a>
+                <img className="w-4" src={newTabIcon} />
               </li>
               <li className="flex gap-2 items-center opacity-60 hover:opacity-100">
-                <div>Instagram</div> <img className="w-4" src={newTabIcon} />
+                <a href="https://www.instagram.com/florar.tech" target="_blank">
+                  Instagram
+                </a>
+                <img className="w-4" src={newTabIcon} />
               </li>
             </ul>
 
-            <button className="transition-all ease-out duration-100 bg-yellow-500 text-yellow-900 shadow-lg shadow-yellow-500/40 hover:shadow-yellow-500/60 text-sm font-semibold px-6 py-2 rounded-full">
-              Contato
-            </button>
+            <a href="#contact-section">
+              <button className="transition-all ease-out duration-100 bg-yellow-500 text-yellow-900 shadow-lg shadow-yellow-500/40 hover:shadow-yellow-500/60 text-sm font-semibold px-6 py-2 rounded-full">
+                Contato
+              </button>
+            </a>
           </div>
         </nav>
       </header>
@@ -94,12 +111,12 @@ const App = () => {
             </span>
           </h1>
 
-          <p className="font-extralight opacity-90 max-w-screen-lg leading-7 mx-auto">
+          <p className="font-extralight opacity-90 max-w-screen-xl leading-7 mx-auto">
             Olá! Me chamo Gabriel e crio soluções com Desenvolvimento
             WEB/Aplicativos e UI/UX Design. Desde quando conheci as duas áreas,
             sabia que queria juntá-las para criar coisas bem legais e fazer a
-            diferença com meu trabalho. E eu amo plantas, daí o nome da minha
-            marca!
+            diferença com meu trabalho. O nome FlorarTech veio da minha paixão
+            por plantas!
           </p>
         </div>
 
@@ -109,7 +126,7 @@ const App = () => {
         >
           <FeatureCard
             iconColor="blue"
-            content="Cada produto é pensado para unir estética e funcionalidade, alinhado às necessidades do cliente e utilizando as tecnologias mais recentes do mercado"
+            content="Cada site/aplicativo é pensado para unir estética e funcionalidade, alinhado às suas necessidades e utilizando as tecnologias mais recentes do mercado"
           />
 
           <FeatureCard
@@ -124,7 +141,7 @@ const App = () => {
 
           <FeatureCard
             iconColor="yellow"
-            content="Acredito muito na transformação que um bom trabalho pode causar e faço o possível para que meus clientes e seus usuários fiquem satisfeitos :)"
+            content="Acredito muito na transformação que um bom trabalho pode causar e faço o possível para que meus clientes e seus usuários fiquem satisfeitos!"
           />
         </div>
       </div>
@@ -203,19 +220,26 @@ const App = () => {
         <div className="">
           <div className="relative">
             <div className="absolute -top-[1rem] text-5xl opacity-30">"</div>
-            <div className="ml-8 mb-4 leading-7">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in
-              leo a libero porttitor ullamcorper at vel nulla. Fusce in
-              convallis elit. Sed dapibus commodo ligula in vulputate. Nullam
-              porttitor non nunc in facilisis. Etiam et varius odio. Ut quis
-              urna orci. Nulla facilisi. Suspendisse gravida neque at mauris
-              pharetra, nec molestie neque euismod. 
+            <div className="ml-8 mb-4 leading-7 font-light">
+              Trabalhar com o Gabriel foi uma excelente experiência. Ele é
+              extremamente responsável, proativo e sempre se dedica ao máximo
+              para entregar tudo dentro do prazo. Conhece muito sobre frontend e
+              design além de sempre entregar soluções criativas e de alta
+              qualidade. Recomendo o Gabriel para qualquer projeto que precise
+              de um profissional competente e comprometido!
             </div>
           </div>
           <div className="font-light ml-8">
             <div className="flex items-center mb-1">
-              <div>Vinicius Brenner</div>
-              <div>Linkedin icon</div>
+              <div className="font-medium">Vinicius Brenner</div>
+              <div>
+                <a
+                  href="https://www.linkedin.com/in/gabriel-mendes-macedo"
+                  target="_blank"
+                >
+                  <img className="w-4 ml-2" src={linkedInLogo} />
+                </a>
+              </div>
             </div>
             <div className="text-xs">Engenheiro de Software</div>
           </div>
